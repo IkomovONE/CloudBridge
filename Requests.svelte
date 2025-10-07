1ddbf488d43c90df91ad27e75a2923b0f727d18c
umandisachini <weerasekaraumandi68@gmail.com>
2025-10-07 23:41:23 +0300

    Add files via upload
    Allows users to submit a data-fetching request to your Golang backend API

---
+++ Requests.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { apiFetch } from '$lib/api';
+  let query = '';
+  let response = '';
+
+  async function sendRequest() {
+    const res = await apiFetch('/api/request', { query });
+    response = JSON.stringify(res, null, 2);
+  }
+</script>
+
+<h2>Request Data</h2>
+<input bind:value={query} placeholder="Enter keyword" />
+<button on:click={sendRequest}>Fetch</button>
+
+<pre>{response}</pre>
