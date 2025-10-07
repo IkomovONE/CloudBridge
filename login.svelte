d8a3de2a6fdfc6b929b8791380b2d02f77017b38
umandisachini <weerasekaraumandi68@gmail.com>
2025-10-07 14:30:55 +0300

    Add files via upload
    Connects with AWS Cognito to handle sign-in/sign-up. Uses Cognito’s hosted UI or Amplify SDK.

---
+++ login.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { user } from '$lib/stores';
+  let email = '', password = '';
+
+  async function handleLogin() {
+    await user.login(email, password);
+  }
+</script>
+
+<h2>Login</h2>
+<input bind:value={email} placeholder="Email" />
+<input type="password" bind:value={password} placeholder="Password" />
+<button on:click={handleLogin}>Login</button>
