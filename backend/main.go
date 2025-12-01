0bfac3af3cabd0ceb9963b5ced58a283d71a0ced
Daniil Komov <daniil_komov@icloud.com>
2025-12-01 03:34:16 +0200

    Updated backend CORS & frontend fetch addresses

--- backend/main.go
+++ backend/main.go
@@ -88,7 +88,7 @@ func main() {
 
 	// CORS middleware - allow your frontend origin(s)
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:3000"},
+		AllowOrigins:     []string{"https://main.d2io5nj8rd4ewj.amplifyapp.com"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
